f1c991284e2c6c7a9025d91f8fcc3ffda019231d
Uday pratap singh <pratapsinghuday75@gmail.com>
2026-07-13 17:26:19 +0530

    Time: 69 ms (14.69%), Space: 31.3 MB (14.29%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -9,7 +9,7 @@ class Solution(object):
         numtorank[sorted_array[0]] = rank
         for i in range(len(sorted_array)):
             if i > 0:
-                if sorted_array[i]>sorted_array[i-1]:
+                if sorted_array[i] not in numtorank:
                     rank += 1
                     numtorank[sorted_array[i]] = rank
                 elif sorted_array[i] == sorted_array[i-1]:
