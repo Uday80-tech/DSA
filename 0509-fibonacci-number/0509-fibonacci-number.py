66e584b14533e45b6f5532847ca17a386b7ecf69
Uday pratap singh <pratapsinghuday75@gmail.com>
2026-07-15 14:47:39 +0530

    Time: 12 ms (85.2%), Space: 12.4 MB (16.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,11 @@
 class Solution(object):
     def fib(self, n):
-        def fibo(n):
-            if n <2:
-                return n
-            else:
-                return fibo(n-1) + fibo(n-2)
-        return fibo(n)
+        if n <2:
+            return n
+        a , b =0,1
+        for i in range(2,n+1):
+            a,b = b,a+b
+        return b
         
+
         
