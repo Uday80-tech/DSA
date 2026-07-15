2a7d13a4bc67fc89017ae582ca185fbc091b7c66
Uday pratap singh <pratapsinghuday75@gmail.com>
2026-07-15 11:43:18 +0530

    Time: 0 ms (100%), Space: 12.2 MB (92.28%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -3,7 +3,5 @@ class Solution(object):
         num1 = (n * (1*2 + (n-1)*2))/2
         num2 = (n * (2*2 + (n-1)*2))/2
         while num1 != 0:
-            rem = num2 % num1
-            num2 = num1
-            num1 = rem
+            num2,num1 = num1,num2 % num1
         return num2
