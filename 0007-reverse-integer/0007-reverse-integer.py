ca609c3585eb748bbe9fd3c4e3a9006cfa2da93e
Uday pratap singh <pratapsinghuday75@gmail.com>
2026-07-15 13:41:21 +0530

    Time: 14 ms (80.93%), Space: 12.4 MB (13.5%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,9 +1,7 @@
 class Solution(object):
     def reverse(self, x):
         nums = 0
-        neg = False
-        Min = -2**31
-        Max = 2**31 -1 
+        neg = False 
 
         if x < 0:
             neg = True
@@ -12,8 +10,8 @@ class Solution(object):
         while x > 0:
             nums = nums * 10 + x % 10
             x = x/10
-            
-        if nums > Max or nums < Min:
+
+        if nums > 2**31 - 1 or nums < -2**31:
             return 0
 
         if neg:
